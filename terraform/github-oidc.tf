500563578e5759aad3ee05b3991e89648ee0a9ff
manitelsa <manisavadatti@gmail.com>
2026-08-17 17:13:17 +0530

    fix: update GitHub OIDC trust and add diagnostics

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -44,7 +44,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:${var.github_repository}:ref:refs/heads/main"
+        "repo:${var.github_repository}:*"
       ]
     }
   }
